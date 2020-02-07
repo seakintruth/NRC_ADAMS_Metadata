233013c1e2c99c92d9341ed265f91937c3a6f2ca
Jeremy D. Gerdes <seakintruth@gmail.com>
2020-02-06 23:12:09 -0500

    Update LinuxSetup.R

--- canidates/LinuxSetup.R
+++ canidates/LinuxSetup.R
@@ -1,7 +1,31 @@
+# Useing this walkthrough
+# https://cran.r-project.org/web/packages/miniCRAN/vignettes/miniCRAN-introduction.html
 pacman::p_load(miniCRAN)
+
+# use Revolution Analytics CRAN mirror
+revolution <- c(CRAN = "http://cran.microsoft.com")
+
+# Specify list of packages to download
 download.file("https://raw.githubusercontent.com/seakintruth/NRC_ADAMS_Metadata/master/canidates/package_list.txt","tmp.list.txt")
 new.package.list <- read.csv("tmp.list.txt")
 file.remove("tmp.list.txt")
 #chooseCRANmirror()
 corrected.list <- c("Package_Name",as.vector(new.package.list$Package_Name))
 list.with.dependancies <- pkgDep(corrected.list)
+# Create temporary folder for miniCRAN
+dir.create(pth <- file.path(tempdir(), "miniCRAN"))
+# Make repo for source and win.binary
+makeRepo(pkgList, path = pth, repos = revolution, type = c("source", "win.binary"))
+
+# List all files in miniCRAN
+package.file.list<- list.files(pth, recursive = TRUE, full.names = FALSE)
+
+# Check for available packages
+pkgAvail(repos = pth, type = "win.binary")[, c(1:3, 5)]
+
+# Install packages with:
+#install.packages(pkgs, 
+#                 repos = paste0("file:///", pth),
+#                 type = "source")
+
+# next is "Adding an older version of a package from CRAN"
